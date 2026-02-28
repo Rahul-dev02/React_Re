ec96fc3da52ee674796ed230d8d8fe7545506166
Rahul-dev02 <rahulroa14320@gmail.com>
2026-02-28 14:13:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const para =
+document.createElement("p")
+
+
+// const text =document.createTextNode("I am Rahul Yadav")
+para.innerHTML="I am Yadav"
+console.log(para)   
+
+document.body.appendChild(para)
