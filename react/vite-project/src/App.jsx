c629b46e9c898fe58a4c98b7b270e5d13314125f
Rahul-dev02 <rahulroa14320@gmail.com>
2026-04-03 00:03:38 +0530

    react

---
+++ react/vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+const Home=()=>{
+
+  return(
+    
+     <h1 className="text-white">Home page</h1>
+    
+  )
+}
+
+export default Home; 
---
+++ react/vite-project/src/main.jsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Home from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+
+    <Home />
+  
+)
